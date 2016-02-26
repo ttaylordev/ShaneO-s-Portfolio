2ee015db764722046080204a561e8bae2f257a70
Kyle Barlow <kbazlo@yahoo.com>
2016-02-26 16:50:21 -0700

    added more images and fixed hovering animation

---
+++ js/directives/header.js
@@ -0,0 +1,10 @@
+angular.module("portfolio")
+    .directive("hedder", function() {
+        return {
+            templateUrl: "views/header.html",
+            restrict: "E",
+
+        }
+
+
+    })
